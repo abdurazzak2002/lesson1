bf08531e26bbf26718a002a044b5954c465480be
abdurazzak2002 <toloev.abi97@gmail.com>
2021-09-18 13:42:25 +0600

    start

---
+++ lesson_1.py
@@ -0,0 +1,95 @@
+#переменные начинаются с Букв или _ или -
+#user_name = 'itrun'
+
+#тип данных
+# 1 - Бинарные переменные 
+#
+
+
+#Проверка типов данных
+# name = 'Abi'
+# print(type(name))
+# user = {
+#     'name':'Abi',
+#     'Age':19,
+#     'from':'Kyrgyz'
+# }
+# print(type(user))
+# num = 16
+# print(type(num))
+# print(user['from'])
+
+#неизменямые типы данных 
+
+# text =  'Hello world'
+# print(id(text))
+# text = 'hello itrun'
+# print(id(text))
+# text.replace('h', 'f')
+# print(text)
+# print(id(text))
+
+#изме9няемые тиипы данных
+
+# product_list = ['apple', 'milk', 'orange']
+# print(id(product_list))
+# product_list.append('meet')
+# print(product_list)
+# print(id(product_list))
+
+# методы - это спец команды, функции для определенного типа данных
+# name = 'abi'
+# print(name.title())
+# print(name.upper())
+# name = name.upper()
+# print(name)
+# print(name.lower())
+
+# name_1 = 'MAX'
+# print(name_1.lower())
+
+# copitalize - переыодит первую букву строки на высший регистр
+
+
+
+
+
+
+
+
+
+
+
+
+# КОНКАТЕНАЦИЯ - это соеденение двух строк
+# first_name = 'Abi'
+# last_name = 'Toloev'
+# print(first_name + ' ' + last_name)
+# middle_name = 'Namatbekovich'
+# print(last_name + ' ' + first_name + ' ' + middle_name)
+# fio = last_name + ' ' + first_name + ' ' + middle_name
+# print(fio)
+# fruit='a'+'p'+'p'+'l'+'e'
+# print(fruit)
+
+
+
+
+
+
+# print('languages:\npython\nC\nJS')
+ 
+
+
+
+# addres = 'Chu 101'
+# print(addres.islower())
+
+# languages = 'python'
+# print(languages.endswith('n'))
+
+
+
+
+
+
